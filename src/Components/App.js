68e8ceb0935c82240b7625ecc22d4546d8b882e6
Bianchi446 <josebianchi221js@gmail.com>
2022-04-13 00:51:06 -0300

    Carousel componente

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,11 +4,15 @@ import { Fragment } from "react";
 import '../scss/__settings.scss'
 import Navbar from "./Navbar"
 import SideBar from "./SideBar";
+import Slider from "./Slider"
+import SliderData from "./SliderImagenes";
 
 const App = () => {
     return(
         <Fragment>
             <Navbar />
+            <Slider slides={SliderData}/>
+        
         </Fragment>
        
        
---
+++ src/Components/Slider.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import reactDom from "react-dom";
+import { Fragment } from "react";
+import { SliderData } from "./SliderImagenes";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import '../styles/Slider.scss'
+
+const Slider = ({slides}) => {
+
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+
+    const nextSlide = () => {
+        setCurrent(current === length -1 ? 0 : current + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length -1 : current -1)
+    }
+
+    if(!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+
+    return(
+        <Fragment>
+            <section className="Slider">
+            <FaArrowAltCircleLeft className="Slider__left--arrow" onClick={prevSlide}/>
+            <FaArrowAltCircleRight className="Slider__right--arrow" onClick={nextSlide}/>
+            {SliderData.map((slide, index) => {
+            return(
+                <div className={index === current ? 'Slider__slide active' : 'slide'} key={index}>
+                {index === current && (<img src={slide.image} alt='imagen 1' className="Slider__image" />)}
+                </div>
+            )
+ })}
+            </section>
+        </Fragment>
+    )
+}
+
+export default Slider
